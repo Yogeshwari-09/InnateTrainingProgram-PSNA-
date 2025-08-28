ae4cf55c216c441fa53a367a12d0f61f87432ee0
Yogeshwari-09 <152641374+Yogeshwari-09@users.noreply.github.com>
2025-08-28 21:56:52 +0530

    To count the number of upper-case characters in a given string.java

---
+++ To count the number of upper-case characters in a given string.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int u=0;
+        for(int i=0;i<=s.length()-1;i++){
+            char c=s.charAt(i);
+            if(c>='A' && c<='Z'){
+                u++;
+            }
+            
+        }
+         System.out.println(u);
+    }
+}
