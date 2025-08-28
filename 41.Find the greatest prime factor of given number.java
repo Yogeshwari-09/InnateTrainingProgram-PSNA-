19324b7093dca6c76b0786ade75d647540165aad
Yogeshwari-09 <152641374+Yogeshwari-09@users.noreply.github.com>
2025-08-28 22:33:23 +0530

    Create 41.Find the greatest prime factor of given number.java

---
+++ 41.Find the greatest prime factor of given number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int greatestPrime = 1;
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                boolean isPrime = true;
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    greatestPrime = i;
+                }
+            }
+        }
+
+        System.out.println(greatestPrime);
+    }
+}
